f6ea3bb27d8f5179ae5b1be5497fa6f57bf5ea79
cecilestaller <c.staller96@gmail.com>
2023-11-20 17:04:03 +0100

    js variablen definiert, werte aus api gezogen und textContent fuer output definiert, alles noch im zweiten then, muss noch in function ausgelagert werden

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+// General Variables
+
+const tempOutput = document.getElementById('tempOutput');
+const cloudOutput = document.getElementById('cloudOutput');
+const obtainedTimeOutput = document.getElementById('obtainedTimeOutput');
+const localTimeOutput = document.getElementById('localTimeOutput');
+const windOutput = document.getElementById('windOutput');
+const humidityOutput = document.getElementById('humidityOutput');
+const sunriseOutput = document.getElementById('sunriseOutput');
+const sunsetOutput = document.getElementById('sunsetOutput');
+// console.log(sunsetOutput);
+
+let weatherDataArr = [];
+
 
 fetch('https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&appid=30fdaf52b46fd3ed234661d059bf0811')
     .then(response => {
@@ -9,5 +23,30 @@ fetch('https://api.openweathermap.org/data/2.5/weather?lat=51.5085&lon=-0.1257&a
     })
     .then(weatherData => {
         console.log(weatherData);
+
+        // Werte aus weatherData(Array) in variablen speichern
+        let temperature = `${Math.round((weatherData.main.temp) / 32)}°C`;
+        let cloudDescription = weatherData.weather[0].description;
+        let currentTime = new Date();
+        let currentLocalTime = currentTime.toLocaleString();
+        let time = new Date();
+        let londonLocaleTime = time.toLocaleTimeString('en-GB', { timeZone: 'Europe/London' });
+        let wind = `speed: ${weatherData.wind.speed} m/s`;
+        let humidity = `${weatherData.main.humidity} %`;
+        let sunriseTime = `${new Date((weatherData.sys.sunrise) * 1000).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })} AM`;
+        let sunsetTime = `${new Date((weatherData.sys.sunset) * 1000).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })} PM`;
+
+        // Text content definieren
+        tempOutput.textContent = temperature;
+        cloudOutput.textContent = cloudDescription;
+        obtainedTimeOutput.textContent = `Obtained at ${currentLocalTime}`;
+        localTimeOutput.textContent = londonLocaleTime;
+        windOutput.textContent = wind;
+        humidityOutput.textContent = humidity;
+        sunriseOutput.textContent = sunriseTime;
+        sunsetOutput.textContent = sunsetTime;
+        
     })
     .catch(error => console.log(error));
+
+
