1a2814d619224356c797f480a02ca6439c478f94
YacineYldrm <yacine@mailbox.org>
2023-11-21 12:17:59 +0100

    dynamische suche implemenitiert. style.css widths und heights überarbeitet. HTML Dokument angepasst

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,56 +11,67 @@ const sunsetOutput = document.getElementById('sunsetOutput');
 
 const locationOutput = document.getElementById('locationOutput');
 const weatherIcon = document.getElementById('weatherIcon');
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
 
 let weatherDataArr = [];
-let city = "Amsterdam";
+let apiKey = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=30fdaf52b46fd3ed234661d059bf0811`;
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=30fdaf52b46fd3ed234661d059bf0811`)
-    .then(response => {
-        console.log(response);
-        if(response.ok === false){
-            throw new Error('etwas ist schief gegangen');
-        }
-        return response.json();
-    })
-    .then(weatherData => {
-        console.log(weatherData);
-        console.log(weatherData.weather[0].icon);
+// durch eingabe nach ort suchen
+const searchLocation = () =>
+{
+    let city = locationInput.value;
+    fetchRequest(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=30fdaf52b46fd3ed234661d059bf0811`);
+};
 
-        // ort als headline ausgeben
-        let cityName = weatherData.name;
-        let country = weatherData.sys.country;
-        locationOutput.textContent = `Weather in ${cityName}, ${country}`;
-        
-        // wetter icons dynamisch anzeigen
-        let iconName = weatherData.weather[0].icon;
-        let weatherDesc = weatherData.weather[0].description;
-        weatherIcon.setAttribute('src', `./assets/img/${iconName}.svg`);
-        weatherIcon.setAttribute('alt', `animated icon: ${weatherDesc}`);
+const fetchRequest = (apiKey) => 
+{
+    fetch(apiKey)
+        .then(response => {
+            console.log(response);
+            if(response.ok === false){
+                throw new Error('etwas ist schief gegangen');
+            }
+            return response.json();
+        })
+        .then(weatherData => {
+            console.log(weatherData);
+            console.log(weatherData.weather[0].icon);
+    
+            // ort als headline ausgeben
+            let cityName = weatherData.name;
+            let country = weatherData.sys.country;
+            locationOutput.textContent = `Weather in ${cityName}, ${country}`;
+            
+            // wetter icons dynamisch anzeigen
+            let iconName = weatherData.weather[0].icon;
+            let weatherDesc = weatherData.weather[0].description;
+            weatherIcon.setAttribute('src', `./assets/img/${iconName}.svg`);
+            weatherIcon.setAttribute('alt', `animated icon: ${weatherDesc}`);
+    
+            // Werte aus weatherData(Array) in variablen speichern
+            let temperature = `${Math.round((weatherData.main.temp) / 32)}°C`;
+            let cloudDescription = weatherData.weather[0].description;
+            let currentTime = new Date();
+            let currentLocalTime = currentTime.toLocaleString();
+            let time = new Date();
+            let londonLocaleTime = time.toLocaleTimeString('en-GB', { timeZone: 'Europe/London' });
+            let wind = `speed: ${weatherData.wind.speed} m/s`;
+            let humidity = `${weatherData.main.humidity} %`;
+            let sunriseTime = `${new Date((weatherData.sys.sunrise) * 1000).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })} AM`;
+            let sunsetTime = `${new Date((weatherData.sys.sunset) * 1000).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })} PM`;
+    
+            // Text content definieren
+            tempOutput.textContent = temperature;
+            cloudOutput.textContent = cloudDescription;
+            obtainedTimeOutput.textContent = `Obtained at ${currentLocalTime}`;
+            localTimeOutput.textContent = londonLocaleTime;
+            windOutput.textContent = wind;
+            humidityOutput.textContent = humidity;
+            sunriseOutput.textContent = sunriseTime;
+            sunsetOutput.textContent = sunsetTime;
+        })
+        .catch(error => console.log(error));
+}
 
-        // Werte aus weatherData(Array) in variablen speichern
-        let temperature = `${Math.round((weatherData.main.temp) / 32)}°C`;
-        let cloudDescription = weatherData.weather[0].description;
-        let currentTime = new Date();
-        let currentLocalTime = currentTime.toLocaleString();
-        let time = new Date();
-        let londonLocaleTime = time.toLocaleTimeString('en-GB', { timeZone: 'Europe/London' });
-        let wind = `speed: ${weatherData.wind.speed} m/s`;
-        let humidity = `${weatherData.main.humidity} %`;
-        let sunriseTime = `${new Date((weatherData.sys.sunrise) * 1000).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })} AM`;
-        let sunsetTime = `${new Date((weatherData.sys.sunset) * 1000).toLocaleTimeString('en-GB', { timeZone: 'Europe/London' })} PM`;
-
-        // Text content definieren
-        tempOutput.textContent = temperature;
-        cloudOutput.textContent = cloudDescription;
-        obtainedTimeOutput.textContent = `Obtained at ${currentLocalTime}`;
-        localTimeOutput.textContent = londonLocaleTime;
-        windOutput.textContent = wind;
-        humidityOutput.textContent = humidity;
-        sunriseOutput.textContent = sunriseTime;
-        sunsetOutput.textContent = sunsetTime;
-        
-    })
-    .catch(error => console.log(error));
-
-
+fetchRequest(apiKey);
